8b9212d3cff82cc2191d9888169498155eb260c7
florianbuchfink <florianbuchfink>
2021-03-24 19:38:35 +0100

    debug false

--- weinmanngoetz/settings.py
+++ weinmanngoetz/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd@)ewl+m(+l3zr#0+(qg+e-xfp!-fn^y3-q&pfi@f$xuqhm_i!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
